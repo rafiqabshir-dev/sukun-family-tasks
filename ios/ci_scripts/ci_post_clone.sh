54abff8ffb433d5e2ea6e325350154cc0764c7d1
Rafiq <rafiq@MacBookPro.lan>
2026-02-09 12:44:35 -0600

    Install Node.js in Xcode Cloud CI script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Install Node.js via Homebrew (not available by default in Xcode Cloud)
+brew install node
+
 # Install Node.js dependencies
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 npm install
